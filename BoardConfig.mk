219086a9cd8076465c3afea574cbf0b0cb7a3fef
ps217 <psyad217@gmail.com>
2018-12-09 09:32:37 +0000

    AEXreborn

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,8 +44,9 @@ BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-TARGET_KERNEL_CONFIG := santoni_defconfig
+TARGET_KERNEL_CONFIG := gabriel_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8937
+TARGET_KERNEL_CLANG_COMPILE := true
 
 # Audio
 AUDIO_FEATURE_ENABLED_ALAC_OFFLOAD := true
